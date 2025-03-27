363ed671c4987fbb03483c69da3616617ed3292f
nawres <nawreshazzez56@gmail.com>
2025-03-27 22:33:02 +0100

    'backend'

---
+++ src/api/tache/routes/tache.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * tache router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::tache.tache');
---
+++ src/api/tache/services/tache.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * tache service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::tache.tache');
